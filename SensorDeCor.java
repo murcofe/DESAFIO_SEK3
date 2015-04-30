bbfbdfa76a478471925dc5d508c66a932505c27e
Murillo Cota Ferreira <murillocot@gmail.com>
2015-04-30 18:26:02 -0300

    Update SensorDeCor.java

--- SensorDeCor.java
+++ SensorDeCor.java
@@ -2,16 +2,20 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.HiTechnicColorSensor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
-
-public class SensorDeCor {
+public class SensorDeCor implements Runnable{
+	
 	EV3ColorSensor sensorDireito = new EV3ColorSensor(SensorPort.S1);
 	HiTechnicColorSensor sensorFrontal = new HiTechnicColorSensor(SensorPort.S2);
 	EV3ColorSensor sensorEsquerdo = new EV3ColorSensor(SensorPort.S3);
+	private int BLACK = 7;
+	public static boolean virarDireita = false;
+	public static boolean virarEsquerda = false;
 	
 	public void Leitura(){
 		while(true){
-			// Leitura do Sensor da Direita
+	/*		// Leitura do Sensor da Direita
 			SampleProvider corDireita = sensorDireito.getColorIDMode();
 			float amostraCorDireita[] = new float[corDireita.sampleSize()];
 			corDireita.fetchSample(amostraCorDireita, 0);
@@ -25,10 +29,24 @@ public class SensorDeCor {
 			SampleProvider corFrontal = sensorFrontal.getColorIDMode();
 			float amostraCorFrontal[] = new float[corFrontal.sampleSize()];
 			corFrontal.fetchSample(amostraCorFrontal, 0);
+	*/		
+			int leituraDireita = sensorDireito.getColorID();
+			int leituraEsquerda = sensorEsquerdo.getColorID();
 			
+			System.out.println("direita = "+leituraDireita);
+			System.out.println("esquerda = "+leituraEsquerda);
+			Delay.msDelay(1000);
 			// Liberar as flags
-			
+			if(leituraDireita == BLACK){
+				virarDireita = true;
+			}
+			if(leituraEsquerda == BLACK){
+				virarEsquerda = true;
+			}
 		}
 	}
-	
+
+	public void run() {
+		Leitura();
+	}
 }
