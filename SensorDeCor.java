4852b00f2b99b55d3b39fef55e5902deac89a4a2
Murillo Cota Ferreira <murillocot@gmail.com>
2015-05-04 17:30:10 -0300

    Update SensorDeCor.java

--- SensorDeCor.java
+++ SensorDeCor.java
@@ -1,48 +1,35 @@
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.HiTechnicColorSensor;
-import lejos.robotics.SampleProvider;
-import lejos.utility.Delay;
+//import lejos.hardware.sensor.HiTechnicColorSensor;
 
-public class SensorDeCor implements Runnable{
-	
+public class SensorDeCor implements Runnable {
+
 	EV3ColorSensor sensorDireito = new EV3ColorSensor(SensorPort.S1);
-	HiTechnicColorSensor sensorFrontal = new HiTechnicColorSensor(SensorPort.S2);
+	//HiTechnicColorSensor sensorFrontal = new HiTechnicColorSensor(SensorPort.S2);
 	EV3ColorSensor sensorEsquerdo = new EV3ColorSensor(SensorPort.S3);
 	private int BLACK = 7;
 	public static boolean virarDireita = false;
 	public static boolean virarEsquerda = false;
-	
-	public void Leitura(){
-		while(true){
-	/*		// Leitura do Sensor da Direita
-			SampleProvider corDireita = sensorDireito.getColorIDMode();
-			float amostraCorDireita[] = new float[corDireita.sampleSize()];
-			corDireita.fetchSample(amostraCorDireita, 0);
-			
-			// Leitura do Sensor da Esquerda
-			SampleProvider corEsquerda = sensorEsquerdo.getColorIDMode();
-			float amostraCorEsquerda[] = new float[corEsquerda.sampleSize()];
-			corDireita.fetchSample(amostraCorEsquerda, 0);
-			
-			// Leitura do Sensor Frontal
-			SampleProvider corFrontal = sensorFrontal.getColorIDMode();
-			float amostraCorFrontal[] = new float[corFrontal.sampleSize()];
-			corFrontal.fetchSample(amostraCorFrontal, 0);
-	*/		
+
+	public void Leitura() {
+		while (true) {
 			int leituraDireita = sensorDireito.getColorID();
 			int leituraEsquerda = sensorEsquerdo.getColorID();
-			
-			System.out.println("direita = "+leituraDireita);
-			System.out.println("esquerda = "+leituraEsquerda);
-			Delay.msDelay(1000);
+
+			System.out.println("direita = " + leituraDireita);
+			System.out.println("esquerda = " + leituraEsquerda);
 			// Liberar as flags
-			if(leituraDireita == BLACK){
+			if (leituraDireita == BLACK) {
 				virarDireita = true;
+			} else {
+				virarDireita = false;
 			}
-			if(leituraEsquerda == BLACK){
+			if (leituraEsquerda == BLACK) {
 				virarEsquerda = true;
+			} else {
+				virarEsquerda = false;
 			}
+			
 		}
 	}
 
