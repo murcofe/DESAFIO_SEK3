dae0539b4ffea4860897b570fe7466ab2518baea
tmpires <tmendespires@gmail.com>
2015-05-07 09:23:09 -0300

    Update SensorUltrassonico.java

--- SensorUltrassonico.java
+++ SensorUltrassonico.java
@@ -1,6 +1,7 @@
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class SensorUltrassonico implements Runnable {
 	public void run() {
@@ -8,12 +9,13 @@ public class SensorUltrassonico implements Runnable {
 	}
 
 	EV3UltrasonicSensor sensorUltrassom = new EV3UltrasonicSensor(SensorPort.S4);
+	public static boolean obstaculoDetectado = false;
+	public static boolean corpoIdentificado = false;
 	private float dis;
+
 	public float getDis() {
 		return dis;
 	}
-
-	public static boolean obstaculoDetectado = false;
 
 	public void Leitura() {
 		while (true) {
@@ -21,12 +23,17 @@ public class SensorUltrassonico implements Runnable {
 			float amostraDistancia[] = new float[distancia.sampleSize()];
 			distancia.fetchSample(amostraDistancia, 0);
 			System.out.println(amostraDistancia[0]);
+			Delay.msDelay(2000);
 			dis = amostraDistancia[0];
 
 			if (dis < 0.05) {
 				obstaculoDetectado = true;
 			} else {
 				obstaculoDetectado = false;
+			}
+			if (dis < 0.14) {
+				corpoIdentificado = true;
+				
 			}
 
 		}
