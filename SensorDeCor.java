08cb60cef7df40319b54ffd2b046bac33d968a16
tmpires <tmendespires@gmail.com>
2015-05-07 09:22:46 -0300

    Update SensorDeCor.java

--- SensorDeCor.java
+++ SensorDeCor.java
@@ -1,35 +1,48 @@
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
-//import lejos.hardware.sensor.HiTechnicColorSensor;
+import lejos.utility.Delay;
 
 public class SensorDeCor implements Runnable {
 
 	EV3ColorSensor sensorDireito = new EV3ColorSensor(SensorPort.S1);
-	//HiTechnicColorSensor sensorFrontal = new HiTechnicColorSensor(SensorPort.S2);
 	EV3ColorSensor sensorEsquerdo = new EV3ColorSensor(SensorPort.S3);
 	private int BLACK = 7;
 	public static boolean virarDireita = false;
 	public static boolean virarEsquerda = false;
+	public static boolean parar = false;
+	public static boolean vEsquerda = false;
+	public static boolean vDireita = false;
 
 	public void Leitura() {
 		while (true) {
 			int leituraDireita = sensorDireito.getColorID();
 			int leituraEsquerda = sensorEsquerdo.getColorID();
 
-			System.out.println("direita = " + leituraDireita);
-			System.out.println("esquerda = " + leituraEsquerda);
 			// Liberar as flags
-			if (leituraDireita == BLACK) {
-				virarDireita = true;
-			} else {
+			if (leituraDireita == BLACK && leituraEsquerda == BLACK) {
+				parar = true;
 				virarDireita = false;
-			}
-			if (leituraEsquerda == BLACK) {
-				virarEsquerda = true;
-			} else {
 				virarEsquerda = false;
+				Delay.msDelay(100);
+			} else {
+				if (leituraDireita == BLACK
+						&& SensorUltrassonico.obstaculoDetectado == false) {
+					virarDireita = true;
+					vEsquerda = true;
+				} else {
+					virarDireita = false;
+					vEsquerda = false;
+				}
+				if (leituraEsquerda == BLACK
+						&& SensorUltrassonico.obstaculoDetectado == false) {
+					virarEsquerda = true;
+					vDireita = true;
+
+				} else {
+					virarEsquerda = false;
+					vDireita = false;
+				}
 			}
-			
 		}
 	}
 
